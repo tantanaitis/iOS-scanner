467f1b2d592c976b68a2c0323415c5076b394527
tantanaitis <titasantanaitis@gmail.com>
2017-10-15 16:16:58 +0300

    capture image and preview implementation

--- eDoc/ViewController.swift
+++ eDoc/ViewController.swift
@@ -9,28 +9,30 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     var captureSession: AVCaptureSession!
     var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var captureDeviceInput: AVCaptureDeviceInput!
+    var photoOutput: AVCapturePhotoOutput!
+    @IBOutlet var screenView: UIView!
     @IBOutlet var previewView: UIView!
+    @IBOutlet var captureButton: UIButton!
+    @IBOutlet var photo: UIImageView!
     
-    override func viewDidLoad() {
-        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            captureSession = AVCaptureSession()
-            captureSession.addInput(input)
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-            videoPreviewLayer.frame = view.layer.bounds
-            previewView.layer.addSublayer(videoPreviewLayer)
-            captureSession.startRunning()
-        } catch {
-            print(error)
-        }
+    @IBAction func captureImage() {
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
     }
     
+    override func viewDidLoad() {
+        styleCaptureButton()
+        captureSession = AVCaptureSession()
+        configureCameraPreviewView()
+        configureCaptureDevice()
+        configurePhotoOutput()
+        captureSession.startRunning()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -40,6 +42,45 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?,
+                 resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Swift.Error?) {
+            
+        if let buffer = photoSampleBuffer, let data = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: buffer, previewPhotoSampleBuffer: nil),
+            let image = UIImage(data: data) {
+            photo.image = image
+        }
+    }
+    
+    func styleCaptureButton() {
+        captureButton.layer.borderColor = UIColor.black.cgColor
+        captureButton.layer.borderWidth = 2
+        captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+    }
+    
+    func configureCameraPreviewView() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer)
+        view.insertSubview(photo, aboveSubview: previewView)
+    }
+    
+    func configureCaptureDevice() {
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do {
+            captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func configurePhotoOutput() {
+        photoOutput = AVCapturePhotoOutput()
+        photoOutput.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecJPEG])], completionHandler: nil)
+        captureSession.addOutput(photoOutput)
     }
     
 }
