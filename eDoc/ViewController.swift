2edd6ad5a529c78823c51922ee71b77cc0e85c25
tantanaitis <titasantanaitis@gmail.com>
2017-10-15 14:09:48 +0300

    camera preview

--- eDoc/ViewController.swift
+++ eDoc/ViewController.swift
@@ -7,8 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    @IBOutlet var previewView: UIView!
+    
+    override func viewDidLoad() {
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession.addInput(input)
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer)
+            captureSession.startRunning()
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
 }
+
 
